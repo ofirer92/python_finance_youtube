a8a6f239ea33d6536244d8de5422cad017ad30d0
ofirer92 <46420629+ofirer92@users.noreply.github.com>
2022-01-06 11:49:05 +0200

    3

--- Project_F/ForNow.py
+++ Project_F/ForNow.py
@@ -6,11 +6,19 @@ import pandas_datareader.data as web
 import pandas as pd
 
 
+style.use('ggplot')
+# start = dt.datetime(2010,1,1)
+# end = dt.datetime.today()
 
+# df =web.DataReader('TSLA','yahoo',start,end)
+# print(df.tail())
+# print(df.columns)
 
-
-
-
+# create csv out of Data frame!
+# df.to_csv('tsla.csv'
+df = pd.read_csv('tsla.csv',parse_dates=True,index_col=0)
+df[['Open','Close']].plot()
+plt.show()
 
 
 
